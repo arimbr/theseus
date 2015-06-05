3a0e55f8d1d41483d3c1b71b66c072ff4411a4c9
arimbr <arimbr@gmail.com>
2015-06-05 17:11:16 +0200

    Create Scrapy project to collect thesis data from theseus.fi

---
+++ collect/collect/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for collect project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'collect'
+
+SPIDER_MODULES = ['collect.spiders']
+NEWSPIDER_MODULE = 'collect.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'collect (+http://www.yourdomain.com)'
