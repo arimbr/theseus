3ccd4824df2812c31430caf570a99f405f59078d
arimbr <arimbr@gmail.com>
2015-06-05 18:01:21 +0200

    Define Thesis fields

--- collect/collect/items.py
+++ collect/collect/items.py
@@ -8,7 +8,15 @@
 import scrapy
 
 
-class CollectItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class Thesis(scrapy.Item):
+    identifier = scrapy.Field()
+    date = scrapy.Field()
+    collections = scrapy.Field()
+    url = scrapy.Field()
+    authors = scrapy.Field()
+    abstract_fi = scrapy.Field()
+    abstract_en = scrapy.Field()
+    language = scrapy.Field()
+    subjects = scrapy.Field()
+    title = scrapy.Field()
+    document = scrapy.Field()
