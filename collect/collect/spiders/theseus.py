81a80fd18dc2ec6087ce5071c09ae2fdcb1538cf
arimbr <arimbr@gmail.com>
2015-06-08 15:18:16 +0200

    Add fields (organization, programme, orientation, abstract_sv) and rename fields (title->titles, document->documents_url)

--- collect/collect/spiders/theseus.py
+++ collect/collect/spiders/theseus.py
@@ -19,12 +19,16 @@ class TheseusSpider(scrapy.Spider):
             thesis['collections'] = record.xpath('.//header/setSpec/text()').extract()
             thesis['url'] = record.xpath('.//metadata/link/@href').extract()
             thesis['authors'] = record.xpath('.//metadata/field[contains(@qualifier, "author")]/@value').extract()
+            thesis['organization'] = record.xpath('.//metadata/field[contains(@element, "organization")]/@value').extract()
+            thesis['programme'] = record.xpath('.//metadata/field[contains(@element, "programme")]/@value').extract()
+            thesis['orientation'] = record.xpath('.//metadata/field[contains(@element, "orientation")]/@value').extract()
             thesis['abstract_fi'] = record.xpath('.//metadata/field[contains(@qualifier, "abstract")][contains(@language, "fi")]/@value').extract()
             thesis['abstract_en'] = record.xpath('.//metadata/field[contains(@qualifier, "abstract")][contains(@language, "en")]/@value').extract()
+            thesis['abstract_sv'] = record.xpath('.//metadata/field[contains(@qualifier, "abstract")][contains(@language, "sv")]/@value').extract()
             thesis['language'] = record.xpath('.//metadata/field[contains(@element, "language")]/@value').extract()
             thesis['subjects'] = record.xpath('.//metadata/field[contains(@element, "subject")]/@value').extract()
-            thesis['title'] = record.xpath('.//metadata/field[contains(@element, "title")]/@value').extract()
-            thesis['document'] = record.xpath('.//metadata/file/@href').extract()
+            thesis['titles'] = record.xpath('.//metadata/field[contains(@element, "title")]/@value').extract()
+            thesis['documents_url'] = record.xpath('.//metadata/file/@href').extract()
             yield thesis
 
         # Crawl next page
