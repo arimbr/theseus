25ce0a690b56339f0fdca6c90406953c76b12187
arimbr <ari.bajo@toucantoco.com>
2017-04-10 12:22:17 +0200

    add pipeline test

--- collect/tests/test_pipelines.py
+++ collect/tests/test_pipelines.py
@@ -1,9 +1,13 @@
 from unittest import TestCase
 
-from ..pipelines import get_topics
+from collect.pipelines import get_topics
 
 
 class PipelinesTest(TestCase):
 
     def test_get_topics(self):
-        pass
+        """It should remove duplicates, remove empty and normalize topics"""
+        keywords = ["Java", "", "hibernate"]
+        subjects = ["java", "sql"]
+        topics = get_topics(keywords, subjects)
+        self.assertCountEqual(topics, ["java", "sql", "hibernate"])
